e66bf5d57c4adbeb84853a76bdc2d67feff0f29a
Kanchan Shirke <kanchanshirke.ks@gmail.com>
2024-01-25 20:08:34 +0530

    added test case for application context

--- src/test/java/com/example/quiz/QuizAppApplicationTests.java
+++ src/test/java/com/example/quiz/QuizAppApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.quiz;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class QuizAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context ) {
+		assertThat(context).isNull();
 	}
 
 }
